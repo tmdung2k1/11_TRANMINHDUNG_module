88c41039313f94979ced61b94ce25a2a57f7fbe9
tmdung2k1 <dung0856028347@gmail.com>
2026-01-01 19:40:08 +0700

    Add Bai_02 WinForms project with greeting button
    Added a new C# Windows Forms project "Bai_02" to the solution. The project includes a main form with a "Click Me" button that shows a greeting message box. Project files, designer, resources, and configuration were set up for .NET Framework 4.8.

---
+++ Bai_02/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Bai_02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_click = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_click
+            // 
+            this.btn_click.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btn_click.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btn_click.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_click.ForeColor = System.Drawing.Color.Red;
+            this.btn_click.Location = new System.Drawing.Point(249, 160);
+            this.btn_click.Name = "btn_click";
+            this.btn_click.Size = new System.Drawing.Size(180, 62);
+            this.btn_click.TabIndex = 0;
+            this.btn_click.Text = "Click Me";
+            this.btn_click.UseVisualStyleBackColor = false;
+            this.btn_click.Click += new System.EventHandler(this.btn_click_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(699, 450);
+            this.Controls.Add(this.btn_click);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_click;
+    }
+}
+
---
+++ Bai_02/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai_02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btn_click_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hi, Welcome to C# programming!", "Greetings", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
